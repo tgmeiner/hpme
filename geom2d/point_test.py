63c476b12cffa11a6842ae7e709a935f9f67e086
tgmeiner <timotheus.gmeiner@gmail.com>
2023-04-07 14:29:59 +0200

    Added segment initialization.

--- geom2d/point_test.py
+++ geom2d/point_test.py
@@ -1,11 +1,13 @@
 import unittest
 
-#test distance between points
 from geom2d.point import Point
 
 class TestPoint(unittest.TestCase):
 
     def test_distance_to(self):
+        '''
+        test distance between points
+        '''
         p= Point(1,2)
         q = Point(4,6)
         expected = 5
---
+++ geom2d/segment.py
@@ -0,0 +1,11 @@
+from geom2d.point import Point
+
+class Segment:
+
+    #initialze segment
+    def _init_(self, start: Point, end: Point):
+        self.start = start
+        self.end = end 
+
+
+        
